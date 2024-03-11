20668bd765f2838a7dffb44de82ab2a5872983e4
SeanWits <162125074+SeanWits@users.noreply.github.com>
2024-03-11 13:51:51 +0200

    Update lab04.js

--- lab4/lab04.js
+++ lab4/lab04.js
@@ -1,5 +1,10 @@
 let title;
 
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+
 async function fetchData() {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
