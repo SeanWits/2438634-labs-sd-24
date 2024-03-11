2d0bd1ed28b5b0880bce3c62b8f5e61874e6ec62
SeanWits <162125074+SeanWits@users.noreply.github.com>
2024-03-11 13:41:04 +0200

    Add files via upload

---
+++ lab4/lab04.js
@@ -0,0 +1,25 @@
+let title;
+
+async function fetchData() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        title = json.title;
+        console.log(json);
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+    }
+}
+
+fetchData();
+
+const btn = document.getElementById('doBtn');
+headingElement = document.getElementById('head');
+
+btn.addEventListener('click', function() {
+    headingElement.textContent = title;
+});
